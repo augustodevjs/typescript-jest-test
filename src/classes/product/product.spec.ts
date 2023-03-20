c13826a3ceb1b624ed93c3a5e8c8657363a2add6
augusto <augustosaaude@gmail.com>
2023-03-20 16:25:21 -0300

    feat: add test to class product

---
+++ src/classes/product/product.spec.ts
@@ -0,0 +1,16 @@
+import { Product } from './product';
+
+const createSut = (name: string, price: number): Product => {
+  return new Product(name, price);
+};
+
+describe('Product', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    const sut = createSut('Camiseta', 50);
+
+    expect(sut).toHaveProperty('name', 'Camiseta');
+    expect(sut).toHaveProperty('price', 50);
+  });
+});
